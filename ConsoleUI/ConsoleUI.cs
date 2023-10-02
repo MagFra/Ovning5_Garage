0d71c44d7538f76c202f954de48905dd38e96313
MagFra <47413425+magfra@users.noreply.github.com>
2023-10-02 16:39:50 +0200

    Add project files.

---
+++ ConsoleUI/ConsoleUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class ConsoleUI
+    {
+        public void WriteLine(string text) => Console.WriteLine(text);
+        public string ReadLine()
+        {
+            string? line;
+            do
+            {
+                line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) { WriteLine("Du måste skriva något."); }
+            } while (string.IsNullOrEmpty(line));
+            return line;
+        }
+    }
+}
---
+++ Garage/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using ConsoleUI;
+
+namespace Garage
+{
+    public class Manager
+    {
+        private ConsoleUI.ConsoleUI uI;
+        public Manager(ConsoleUI.ConsoleUI cui) { this.uI = cui; }
+        public int Menu()
+        {
+            StringBuilder menuStringBuilder = new StringBuilder();
+            menuStringBuilder.AppendLine("1. jkhdh.");
+            menuStringBuilder.AppendLine("2. jkhdh.");
+            menuStringBuilder.AppendLine("3. jkhdh.");
+            menuStringBuilder.AppendLine("0. Anvsluta.");
+            string menuText = menuStringBuilder.ToString();
+            uI.WriteLine(menuText);
+            return 0;
+        }
+    }
+}
