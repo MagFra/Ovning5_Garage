97ef2e2a435ac2a551a996980f3b9fabe3d627e2
MagFra <47413425+MagFra@users.noreply.github.com>
2023-10-06 09:50:24 +0200

    Byta dator!

---
+++ Garage/NewVehicleCreator.cs
@@ -0,0 +1,35 @@
+﻿using Garage.UI;
+using Garage.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class NewVehicleCreator
+    {
+        private MenuHandler menu;
+        public NewVehicleCreator(MenuHandler menu)
+        {
+            this.menu = menu;
+        }
+        public IVehicle CreateNewVehicle()
+        {
+            StringBuilder menuStringBuilder = new StringBuilder();
+            menuStringBuilder.AppendLine("Välg en fordonstyp!\n");
+            menuStringBuilder.AppendLine("1. Flygplan.\n");
+            menuStringBuilder.AppendLine("2. Båt.\n");
+            menuStringBuilder.AppendLine("3. Buss.\n");
+            menuStringBuilder.AppendLine("4. Bil.\n");
+            menuStringBuilder.AppendLine("5. Motorcykel.\n");
+            menu.ViewMenuText(menuStringBuilder:  menuStringBuilder);
+            int chois = menu.RetrieveMenuChoice();
+
+
+
+            return null!;
+        }
+    }
+}
